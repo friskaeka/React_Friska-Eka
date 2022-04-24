995ce4bc96b6e685f23254c51dd635bd87286e01
affanharits7 <affanharits7@gmail.com>
2022-04-24 07:30:15 +0700

    update code

---
+++ 19_Introduction Algorithm/Praktikum/prak.js
@@ -0,0 +1,31 @@
+const BilanganPrima = (x) =>{
+    for(let i = 2; i<x; i++){
+        if(x % i == 0){
+            console.log("input = " + x +" bukan prima")
+            break
+        }
+        console.log("input = " + x +"   prima")
+        break
+} }
+BilanganPrima(3)
+BilanganPrima(4)
+BilanganPrima(10)
+BilanganPrima(17)
+
+const lampu = (N) =>{
+  
+  let awal = 1
+  let n = 4
+  let isActive  = false
+  
+  console.log(`Lampu mati`)
+  
+  for(let i = awal; i <= n; i++){
+    if((N % i) === 0){
+      isActive = !isActive
+    }
+  }
+  console.log(`${N}   = ${isActive ? "Menyala":"Mati"}`)
+}
+lampu(5)
+
